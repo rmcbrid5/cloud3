d0334352f294ed3e6c759ca7b94678d0f984e271
Michael Tassone <michaeltassone@Michaels-MacBook-Pro-2.local>
2019-03-28 15:54:53 -0400

    first commit

---
+++ Part1/app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from pymongo import MongoClient
+import requests
+import json
+# pprint library is used to make the output look more pretty
+from pprint import pprint
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+myclient = MongoClient("mongodb://localhost:27017/")
+mydb = myclient["cloudapp"]
+
+mycol = mydb["messages"]
+# Issue the serverStatus command and print the results
+
+# App config.
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+
+class ReusableForm(Form):
+    name = TextField('Message:', validators=[validators.required()])
+
+
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    form = ReusableForm(request.form)
+
+
+    print(form.errors)
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'Retreive':
+            messages = requests.get('http://localhost:50001/retreive')
+            messages = json.loads(messages.text)
+            for key, value in messages.items():
+                flash('Message ' + str(key) + " : " + str(value))
+        elif request.form['submit_button'] == 'Submit':
+            name = request.form['name']
+            print(name)
+
+            if form.validate():
+                # Save the comment here.
+                data = {'message': name }
+
+                # sending post request and saving response as response object
+                r = requests.post(url='http://localhost:50001/send', data=data)
+
+            else:
+                flash('All the form fields are required. ')
+            pass  # do something else
+        else:
+            pass
+
+        name = request.form['name']
+        print(name)
+
+    return render_template('hello.html', form=form)
+
+if __name__ == "__main__":
+    app.run()
---
+++ rianaAssignment3Pt2/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from pymongo import MongoClient
+from flask import jsonify
+# pprint library is used to make the output look more pretty
+from pprint import pprint
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+myclient = MongoClient("mongodb://localhost:27017/")
+mydb = myclient["cloudapp"]
+
+mycol = mydb["messages"]
+# Issue the serverStatus command and print the results
+
+# App config.
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+
+class ReusableForm(Form):
+    name = TextField('Message:', validators=[validators.required()])
+
+
+@app.route("/send", methods=['GET', 'POST'])
+def send():
+    data = request.get_data()
+    print(data)
+    message = data.decode("utf-8").split("=")[1]
+    result = mycol.insert_one({'message': message})
+    form = ReusableForm(request.form)
+    return render_template('hello.html', form=form)
+
+@app.route("/retreive", methods=['GET', 'POST'])
+def retreive():
+    messages = {}
+    count = 0
+    for x in mycol.find():
+        count += 1
+        messages[str(count)] = x['message']
+    return jsonify(messages)
+
+
+if __name__ == '__main__':
+    app.run(port='50001')
